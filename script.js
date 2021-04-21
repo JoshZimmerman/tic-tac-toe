b6ab7a39c1cd39a1b9bfd261cd28a0fc87445046
Josh Zimmerman <jz1179@gmail.com>
2021-04-20 20:15:24 -0400

    fixed logo

--- script.js
+++ script.js
@@ -4,20 +4,33 @@ const gameBoard = (function () {
 })();
 
 const displayController = (function () {
+  //methods here should only be concerned with updating visuals on DOM elements
+
   //cache DOM
   const gridDiv = document.querySelector(".game-board");
   let gridSquares = gridDiv.childNodes;
 
-  function setEvents() {
-    gridSquares.forEach( square => {square.addEventListener ('click', function(e) {
+  return {
+    gridSquares:gridSquares,
+  }
+})();
+
+const eventController = (function () {
+  //move all event set up procedures into eventController module
+
+  function setSquares() {
+    displayController.gridSquares.forEach( square => {square.addEventListener ('click', function(e) {
       console.log(`Square ${e.target.dataset.index} was clicked!`);
+      //call private function to update square with players mark to gameState
+      //array and rerender gameboard
       });
     });
   }
 
-  return {
-    setEvents: setEvents
-  };
+  setSquares();
+//  return {
+//    setSquares: setSquares
+//  };
 })();
 
-displayController.setEvents();
+//eventController.setSquares();
