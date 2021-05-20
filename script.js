086dfc486586428fea48906e3a09b68c35279b8c
Josh Zimmerman <jz1179@gmail.com>
2021-05-19 23:24:05 -0400

    Continueing game

--- script.js
+++ script.js
@@ -3,67 +3,106 @@ const gameBoard = (function () {
                '','','',
                '','','']
   //console.log(gameState);
-  const getBoard = () => {
+  function getBoard() {
     return board;
-  };
-  const resetBoard = () => {
-    board = board.map(square => square = '');
-    console.log(board);
   }
-  const setSquare = (mark, index) => {
+
+  function resetBoard() {
+    board = board.map(square => square = '');
+  }
+
+  function setSquare(mark, index) {
     board[index] = mark;
-    console.log(board);
-    displayController.render(board);
+  }
+
+  function getSquare(index) {
+    if (index > board.length) {
+      return;
+    }
+    return board[index];
   }
 
   return {
     getBoard,
     setSquare,
-    resetBoard
+    resetBoard,
+    getSquare
   }
 })();
 
 const Player = (sign) => {
+
   let playerSign = sign;
 
+  const getSign = () => {
+    return playerSign;
+  }
 
-
+  return { 
+    getSign 
+  }
 }
+
 const displayController = (function () {
   //methods here should only be concerned with updating visuals on DOM elements
 
   //cache DOM
   const gridDiv = document.querySelector(".game-board");
-  let gridSquares = document.querySelectorAll(".square");
+  const gridSquares = document.querySelectorAll(".square");
+  const message = document.querySelector("#message");
   console.log(gridSquares);
 
+  function setSquares() {
+    gridSquares.forEach( square => {square.addEventListener ('click', function(e) {
+      console.log(`Square ${e.target.dataset.index} was clicked!`);
+      //call private function to update square with players mark to gameState
+      gameController.playRound(`${e.target.dataset.index}`);
+      //array and rerender gameboard
+      });
+    });
+  }
+
+  
   const render = function(board) {
     for(let i=0; i<board.length; i++) {
       gridSquares.item(i).textContent = board[i]
     }
   }
 
+  const setMessage = (str) => {
+    message.textContent = str;
+  }
+
+  setSquares();
+
   return {
     gridSquares,
+    setMessage,
     render
   }
 })();
 
-const eventController = (function () {
-  //move all event set up procedures into eventController module
+const gameController = (function () {
+  //move all event set up procedures into gameController module
+  const player1 = Player('X');
+  const player2 = Player('O');
 
-  function setSquares() {
-    displayController.gridSquares.forEach( square => {square.addEventListener ('click', function(e) {
-      console.log(`Square ${e.target.dataset.index} was clicked!`);
-      //call private function to update square with players mark to gameState
-      //array and rerender gameboard
-      });
-    });
+  let player1Turn = true;
+
+  function toggleTurn() {
+    player1Turn ? player1Turn = false : player1Turn = true;
   }
 
-  setSquares();
+  const playRound = (index) => {
+    console.log(index)
+  }
+
+  return {
+    playRound
+  }
+  
   //Might need to merge display and event controllers if too much code
   //is duplicated or shared in global
 })();
 
-//eventController.setSquares();
+//gameController.setSquares();
